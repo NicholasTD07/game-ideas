19bf59ce5d5356cabafe66079e4aac18c9457d8c
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-09-03 19:43:05 +1000

    mockup data wing v1, really rough tho...

---
+++ data-wing-mock/Assets/Game.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	private float startedAt;
+
+	// Use this for initialization
+	void Start () {
+		startedAt = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
